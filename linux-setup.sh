f89da2cbdf1144b927fd7caea0ed8dd77c564a21
Lucas Moreira <l.moreira@Live.ca>
2017-02-21 20:46:39 -0500

    REmoved chrome because.. because its going to conflict with stuff..

--- linux-setup.sh
+++ linux-setup.sh
@@ -60,20 +60,6 @@ then
 else
   error_handle "SUDO Failed to update Apt-Get...you are not ready for the singularity."
 
-#[ Install Chrome ]
-clear
-echo
-echo "${YELLOW}[ INSTALLING CHROME MASTER OF ALL BROWSERS ]${NC}"
-sleep 2s
-sudo sh -c 'echo "deb http://dl.google.com/linux/deb/ stable non-free main" >> ~/etc/apt/sources.list'
-sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sudo apt-get update
-#may be google-chrome-beta
-sudo apt-get install google-chrome
-clear
-echo "${GREEN}[ Successfully Installed Chrome ]${NC}"
-sleep 2s
-
 # [ Check for GIT installation & Configuration ]
 if git --version
 then
